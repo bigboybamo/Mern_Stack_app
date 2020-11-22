c60d04aa0c5fb0c98584685a62b1e8adca548a6b
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2020-11-22 14:00:19 +0100

    added CRUD endpoints

--- backend/routes/perfume.route.js
+++ backend/routes/perfume.route.js
@@ -1,10 +1,10 @@
 const express = require('express')
- const router = express.Router()
+const router = express.Router()
+//const mongoose = require('mongoose')
 
 const Perfume = require('../Models/perfume');
 
 // CREATE Perfume
-
 router.post('/perfume', (req, res) => {
   const perfume = new Perfume(req.body)
   perfume.save()
@@ -12,7 +12,6 @@ router.post('/perfume', (req, res) => {
     () => {
       res.status(201).json({
       message: 'Perfume saved successfully!'
-       
       })
     }
   ).catch(
@@ -21,6 +20,62 @@ router.post('/perfume', (req, res) => {
         error: error
       })
     })
+})
+
+//Get every Fragrance in the database
+router.get('/perfume', (req, res, next)=>{
+  Perfume.find((error, data) => {
+    if(error) {
+      return next(error)
+    }
+    else {
+      res.json(data)
+    }
+  })
+})
+
+//Get a fragrance by id
+router.get('/perfume/:id', (req, res, next)=>{
+  Perfume.findById(req.params.id, (error, data) => {
+    if(error) {
+      return next(error)
+    }
+    else {
+      res.json(data)
+    }
+  })
+})
+
+
+//Update a fragrance using id as a search parameter
+router.put('/update-perfume/:id', (req, res,next) =>{
+  Perfume.findByIdAndUpdate(req.params.id, {
+    $set: req.body
+  }, (error, data) => {
+    if(error) {
+      return next(error)
+    }
+    else{
+      res.json(data)
+      console.log('Perfume updated successfully')
+    }
+  })
+})
+
+
+//Delete a particular fragrance using id as a search parameter
+router.delete('/delete-perfume/:id', (req, res, next)=>{
+  Perfume.findByIdAndRemove(req.params.id, (error, data) => {
+    if(error){
+      return next(error)
+    }
+    else{
+      res.status(200).json({
+        message: data
+      })
+      console.log('fragrance deleted')
+    }
+  })
 })
 
 
