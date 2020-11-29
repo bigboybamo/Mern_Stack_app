eeb8aa7efb3ae6a5fdcb14ca23ced9fe47449c0a
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2020-11-29 15:38:48 +0100

    Formatting with eslint and prettier

--- src/App.js
+++ src/App.js
@@ -6,36 +6,50 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-import Show from './components/Show';
-
+import Show from "./components/Show";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import home from "./components/home"
+import home from "./components/home";
 import createPerfume from "./components/createPerfume";
 import perfumeList from "./components/perfumeList";
 
-
 function App() {
-  return (<Router>
-    <div className="App">
-    <Container>
-  <Row>
-    <Col className="logo"><b><Link to ="/" className="link">PerfumeHouse</Link></b></Col>
-    <Col className="right-content"><div className="grid-child purple"><Link to="/perfumeList" className="link">See All Perfumes</Link></div><div className="link"><Link to="/createPerfume">Submit a perfume</Link></div></Col>
-  </Row>
-  </Container>
-   <home/>
+  return (
+    <Router>
+      <div className="App">
+        <Container>
+          <Row>
+            <Col className="logo">
+              <b>
+                <Link to="/" className="link">
+                  PerfumeHouse
+                </Link>
+              </b>
+            </Col>
+            <Col className="right-content">
+              <div className="grid-child purple">
+                <Link to="/perfumeList" className="link">
+                  See All Perfumes
+                </Link>
+              </div>
+              <div className="link">
+                <Link to="/createPerfume">Submit a perfume</Link>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+        <home />
 
-      <Switch>
-      <Route exact path="/" component={home} />
-      <Route path="/perfumeList" component={perfumeList} />
-      <Route path="/createPerfume" component={createPerfume} />
-      <Route path='/show/:id' component={Show} />
+        <Switch>
+          <Route exact path="/" component={home} />
+          <Route path="/perfumeList" component={perfumeList} />
+          <Route path="/createPerfume" component={createPerfume} />
+          <Route path="/show/:id" component={Show} />
         </Switch>
-      
-    </div>
-  </Router>);
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Show.js
+++ src/components/Show.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 // import { Link } from 'react-router-dom';
 
 class Show extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      perfume: {}
+      perfume: {},
     };
   }
 
   componentDidMount() {
-    axios.get('http://localhost:4000/perfume/'+this.props.match.params.id)
-      .then(res => {
+    axios
+      .get("http://localhost:4000/perfume/" + this.props.match.params.id)
+      .then((res) => {
         this.setState({ perfume: res.data });
         console.log(this.state.perfume);
       });
@@ -33,11 +33,10 @@ class Show extends Component {
         <div className="panel panel-default">
           <div className="panel-heading">
             <h3 class="panel-title">
-             Perfume Name: {this.state.perfume.perfumeName}
+              Perfume Name: {this.state.perfume.perfumeName}
             </h3>
           </div>
           <div class="panel-body">
-           
             <dl>
               <dt>Brand:</dt>
               <dd>{this.state.perfume.brand}</dd>
@@ -46,11 +45,13 @@ class Show extends Component {
               <dt>Submitted by:</dt>
               <dd>{this.state.perfume.yourName}</dd>
               <dt>Why {this.state.perfume.yourName} loves this perfume:</dt>
-              <dd><i>"{this.state.perfume.luvFragrance}"</i></dd>
+              <dd>
+                <i>"{this.state.perfume.luvFragrance}"</i>
+              </dd>
               <dt>Price($):</dt>
               <dd>${this.state.perfume.price}</dd>
             </dl>
-              </div>
+          </div>
         </div>
       </div>
     );
